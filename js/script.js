6fd0b35d938b4462cc7bbcd92a91ef78d441b420
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2023-09-13 16:28:50 +0200

    [add] btn js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 /* 1. stampo tutte le immagini dentro a items-wrapper
   2.stampo la mia array con dentro le immagini
   3.metto in un array tutti gli elemnti con la classe 'oggetto'
-  4. rimuovo la classe hide del primo elemnto dell'array 'immaginiList'
-  5.applichiamo al click le diverse funzioni 
+  4. rimuovo la classe hide del primo elemnto dell'array 'immaginiList' 
+  5. assegnamo le variabili dei bottoni 
+  6.applichiamo al click le diverse funzioni 
     a.aggiungiamo la classe hide all'elemento corrente
     b. incrementiamo il contatore
     c. tolgo la class hide all'elemento corrente
@@ -16,13 +17,19 @@
 
 //variabili
 const itemsWrapper = document.querySelector('.items-wrapper');
+
+
+//5.
+const btnAvanti =    document.querySelector('.up');
+const btnIndietro =  document.querySelector('.down');
+
 //.2
 const immaginiList = [
-  '../consegna/img/01.webp',
-  '../consegna/img/02.webp',
-  '../consegna/img/03.webp',
-  '../consegna/img/04.webp',
-  '../consegna/img/05.webp',
+                      '../consegna/img/01.webp',
+                      '../consegna/img/02.webp',
+                      '../consegna/img/03.webp',
+                      '../consegna/img/04.webp',
+                      '../consegna/img/05.webp',
 ]
 
 const contaImg = 0;
@@ -31,16 +38,41 @@ const contaImg = 0;
 //1.
 for(let i = 0; i < immaginiList.length; i++){
   const immagine = immaginiList[i];
+  
   itemsWrapper.innerHTML += `<img src="${immagine}" class="oggetto hide">`
 }
 
 //3.
-const oggettoColl = document.getElementsByClassName('oggetto');
+let oggettoColl = document.getElementsByClassName('oggetto');
 
 console.log(oggettoColl[0]);
 
 //4.
 oggettoColl[contaImg].classList.remove('hide');
+
+//6.
+
+btnAvanti.addEventListener('click', function(){
+  //a.
+  oggettoColl[contaImg].classList.add('hide');
+  //b.
+  contaImg++;
+  //c.
+  oggettoColl[contaImg].classList.remove('hide');
+
+})
+
+btnIndietro.addEventListener('click', function(){
+  //a.
+  oggettoColl[contaImg].classList.add('hide');
+  //b.
+  contaImg++;
+  //c.
+  oggettoColl[contaImg].classList.remove('hide');
+  
+  })
+
+
 
 
 
