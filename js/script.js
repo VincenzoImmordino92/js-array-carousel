e977321b627b6cff68417b6f723539758231118f
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2023-09-13 17:40:59 +0200

    [add] comment

--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@
   3.metto in un array tutti gli elemnti con la classe 'oggetto'
   4. rimuovo la classe hide del primo elemnto dell'array 'immaginiList' 
   5. assegnamo le variabili dei bottoni 
-  6.applichiamo al click le diverse funzioni 
+  6./7.applichiamo al click le diverse funzioni per i bottoni sia avanti che indietro
     a.aggiungiamo la classe hide all'elemento corrente
     b. incrementiamo il contatore
     c. tolgo la class hide all'elemento corrente
+    8. aggiungiamo di default al bottone down di inizio pagina la class hide
+    9.aggiungiamo al click del bottoneavanti rimuoviamo la classe hide al bottoneIndietro
+    10.nell'ultimo elemento del mia array applichiamo al bottoneAvanti la classe hide
+    11.
+
 
 
 
@@ -23,16 +28,19 @@ const itemsWrapper = document.querySelector('.items-wrapper');
 const btnAvanti =    document.querySelector('.up');
 const btnIndietro =  document.querySelector('.down');
 
+//8.
+btnIndietro.classList.add('hide');
+
 //.2
 const immaginiList = [
-                      '../consegna/img/01.webp',
-                      '../consegna/img/02.webp',
-                      '../consegna/img/03.webp',
-                      '../consegna/img/04.webp',
-                      '../consegna/img/05.webp',
+                      './consegna/img/01.webp',
+                      './consegna/img/02.webp',
+                      './consegna/img/03.webp',
+                      './consegna/img/04.webp',
+                      './consegna/img/05.webp',
 ]
 
-const contaImg = 0;
+let contaImg = 0;
 
 
 //1.
@@ -51,7 +59,6 @@ console.log(oggettoColl[0]);
 oggettoColl[contaImg].classList.remove('hide');
 
 //6.
-
 btnAvanti.addEventListener('click', function(){
   //a.
   oggettoColl[contaImg].classList.add('hide');
@@ -60,16 +67,30 @@ btnAvanti.addEventListener('click', function(){
   //c.
   oggettoColl[contaImg].classList.remove('hide');
 
-})
+  //9.
+  btnIndietro.classList.remove('hide')
+  // controllo se il mio contaImg è uguale all'ultimo elemento aggiungo al bottoneavanti la classe hide
+  if(contaImg === oggettoColl.length - 1){
+    btnAvanti.classList.add('hide')
+  }
 
+})
+//7.
 btnIndietro.addEventListener('click', function(){
   //a.
   oggettoColl[contaImg].classList.add('hide');
   //b.
-  contaImg++;
+  contaImg--;
   //c.
   oggettoColl[contaImg].classList.remove('hide');
   
+  //10.
+  btnAvanti.classList.remove('hide');
+
+    // controllo se il mio contaImg è uguale al primo elemento aggiungo al bottoneIndietro la classe hide
+  if(contaImg === 0){
+    btnIndietro.classList.add('hide');
+  }
   })
 
 
